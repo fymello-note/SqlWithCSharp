9dca0878e6bf25b99491c1500e9a79cd8f8ab410
Matteo Massimello <matteo.massimello@gmail.com>
2026-01-15 17:52:42 +0100

    feat

--- Esercitazione.sql
+++ Esercitazione.sql
@@ -16,8 +16,28 @@ CREATE TABLE [Question] (
 	[Text] VARCHAR(max) NOT NULL
 );*/
 
-/*INSERT INTO dbo.[User](FirstName, LastName, UserName, [Password]) VALUES ('Matteo', 'Massimello', 'Matteo.Massimello', 'Matteobest1!');
+/*INSERT INTO dbo.[User](FirstName, LastName, UserName, [Password]) VALUES ('Matteo', 'Massimello', 'Matteo.Massimello', 'Matteobest1!');*/
 
-select * from [User];*/
+select * from [User];
 
 INSERT INTO [Question]([Text]) VALUES ('Hai la patente?');
+
+CREATE TABLE Answer (
+	AnswerId INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
+	QuestionId SMALLINT NOT NULL FOREIGN KEY REFERENCES [Question] (QuestionId),
+	UserId INT NOT NULL FOREIGN KEY REFERENCES [User] (UserId),
+	Answer BIT NOT NULL
+);
+
+ALTER TABLE Answer DROP COLUMN Answer
+ALTER TABLE Answer ADD AnswerValue bit not null
+
+alter table Answer ADD CONSTRAINT UQ_Answer_QuestionId_UserId UNIQUE (QuestionId, UserId);
+
+SELECT * FROM Answer;
+
+TRUNCATE TABLE Answer;
+
+UPDATE [User] SET Flag_Form = 0;
+
+INSERT INTO dbo.[User](FirstName, LastName, UserName, [Password]) VALUES ('Ciccio', 'Merlino', 'Ciccio.Merlino', '123');
